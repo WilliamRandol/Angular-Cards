338d355e7275cbc19b20f8132dccbb5c814de71b
WilliamRandol <william.randol@gmail.com>
2014-08-15 22:48:18 -0600

    purdyness

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,43 +1,49 @@
 'use strict';
 
 angular.module('cards', [])
-  .controller('cardsCtrl', function($scope) {
-  
-	function buildDeck(jokers, decks){
-		var suits = [ 'spade', 'club', 'diamond', 'heart'];
-		var values = Number.range(1,13);
-		var fullDeck = [];
-		suits.forEach(function(suit){
-		  values.every(function(value){
-			fullDeck.push({selected: false});
-			fullDeck[fullDeck.length - 1][suit + ' value-' + value.pad(2)] = true;
-		  });
-		});
-		if (jokers) {
-			fullDeck.push({'joker value-l': true, selected: false});
-			fullDeck.push({'joker value-b': true, selected: false});
-		}
-		if (decks) {
-			var newDeck = fullDeck;
-			for (var i = 0; i < decks; i++){
-				fullDeck = fullDeck.add(newDeck);
-			}
-		}
-		return fullDeck;
-	}
-	$scope.fullDeck = buildDeck(true);
+	.controller('cardsCtrl', function ($scope) {
 
-	$scope.toggleSelected = function(card){
-		card.selected = !card.selected;
-	}
-	
-  })
-  .directive('card', function() {
-    return {
-	  scope: {
-	  },
-	  restrict: 'E',
-      templateUrl: 'assets/js/template/card.html',
-    };
-  });
-  
+		function buildDeck(jokers, decks) {
+			var suits = ['spade', 'club', 'diamond', 'heart'],
+				values = Number.range(1, 13),
+				fullDeck = [];
+			suits.forEach(function (suit) {
+				values.every(function (value) {
+					fullDeck.push({
+						selected: false
+					});
+					fullDeck[fullDeck.length - 1][suit + ' value-' + value.pad(2)] = true;
+				});
+			});
+			if (jokers) {
+				fullDeck.push({
+					'joker value-l': true,
+					selected: false
+				});
+				fullDeck.push({
+					'joker value-b': true,
+					selected: false
+				});
+			}
+			if (decks) {
+				var newDeck = fullDeck;
+				for (var i = 1; i < decks; i++) {
+					fullDeck = fullDeck.add(angular.copy(newDeck));
+				}
+			}
+			return fullDeck;
+		}
+		$scope.fullDeck = buildDeck(true, 1);
+
+		$scope.toggleSelected = function (card) {
+			card.selected = !card.selected;
+		};
+
+	})
+	.directive('card', function () {
+		return {
+			scope: {},
+			restrict: 'E',
+			templateUrl: 'assets/js/template/card.html'
+		};
+	});
